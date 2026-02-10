ff773e13cd2ee2bf5fb6a7ab2feadb4461ed9d08
aydansh24 <aydansh24@csu.fullerton.edu>
2026-02-09 16:54:58 -0800

    Update burger.py

--- burger.py
+++ burger.py
@@ -2,10 +2,16 @@ import pygame
 from food import Food
 
 class Burger(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos=(0,0)):
         super().__init__()
-        self.image = pygame.image.load('burger.jpg').convert_alpha()
+        self.image = pygame.image.load('burger.jpg')
         self.image = pygame.transform.scale_by(self.image, 0.07)
         self.ingredients = ["buns", "patty", "lettuce", "tomato", "cheese"]
         self.cost = 5
         self.rect = self.image.get_rect()
+
+    def get_rect(self):
+        return self.rect
+
+    def get_pic(self):
+        return self.image
