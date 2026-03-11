c90764b593f1532ec73686463009ab9651c488ef
aydansh24 <aydansh24@csu.fullerton.edu>
2026-03-11 15:59:27 -0700

    Update server.py
    aydan hotspot

--- Multiplayer/server.py
+++ Multiplayer/server.py
@@ -3,7 +3,7 @@ import socket
 from _thread import *
 from player import Player
 
-server = "10.67.38.202"
+server = "172.20.10.2"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -59,4 +59,5 @@ while True:
         currentPlayer += 1
     else:
         conn.close()
+
 
