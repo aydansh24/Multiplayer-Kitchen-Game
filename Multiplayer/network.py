2346fdf17b5c7544acbf6c417db0c44f0308bcde
aydansh24 <aydansh24@csu.fullerton.edu>
2026-03-16 16:24:30 -0700

    Update network.py

--- Multiplayer/network.py
+++ Multiplayer/network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.1.96"
+        self.server = "10.67.38.202"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.player_id = self.connect()
@@ -26,5 +26,3 @@ class Network:
             return pickle.loads(self.client.recv(4096))
         except socket.error as e:
             print(e)
-
-
