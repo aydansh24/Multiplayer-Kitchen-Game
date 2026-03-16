328c2db8b55e7c5b447551c24b1bb159ed168078
aydansh24 <aydansh24@csu.fullerton.edu>
2026-03-16 16:24:11 -0700

    Update player.py

--- Multiplayer/player.py
+++ Multiplayer/player.py
@@ -8,6 +8,7 @@ class Player:
         self.height = height
         self.color = color
         self.vel = 6
+        self.inventory = None
 
     def get_rect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
@@ -15,39 +16,36 @@ class Player:
     def draw(self, win, img):
         win.blit(img, (self.x, self.y))
 
+        if self.inventory:
+            pygame.draw.circle(win,(255,255,255),(self.x+25,self.y-10),8)
+
     def move(self, collisions):
         keys = pygame.key.get_pressed()
 
         dx = 0
         dy = 0
 
-        if keys[pygame.K_LEFT]:
-            dx -= self.vel
-        if keys[pygame.K_RIGHT]:
-            dx += self.vel
-        if keys[pygame.K_UP]:
-            dy -= self.vel
-        if keys[pygame.K_DOWN]:
-            dy += self.vel
+        if keys[pygame.K_LEFT]:  dx -= self.vel
+        if keys[pygame.K_RIGHT]: dx += self.vel
+        if keys[pygame.K_UP]:    dy -= self.vel
+        if keys[pygame.K_DOWN]:  dy += self.vel
 
-        # Move horizontally
         self.x += dx
         player_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
         for rect in collisions:
             if player_rect.colliderect(rect):
-                if dx > 0:  # moving right
+                if dx > 0:
                     self.x = rect.left - self.width
-                elif dx < 0:  # moving left
+                elif dx < 0:
                     self.x = rect.right
 
-        # Move vertically
         self.y += dy
         player_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
         for rect in collisions:
             if player_rect.colliderect(rect):
-                if dy > 0:  # moving down
+                if dy > 0:
                     self.y = rect.top - self.height
-                elif dy < 0:  # moving up
+                elif dy < 0:
                     self.y = rect.bottom
