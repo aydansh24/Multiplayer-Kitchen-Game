e1ce48c15d8700589cec8b3f13e76da608fdbaaf
aydansh24 <aydansh24@csu.fullerton.edu>
2026-03-26 13:20:28 -0700

    temp network server change for testing

--- Multiplayer/network.py
+++ Multiplayer/network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.67.38.202"
+        self.server = "172.20.10.2"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.player_id = self.connect()
